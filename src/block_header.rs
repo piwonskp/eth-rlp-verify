fd004549b2c7f8eb5a3dd9a062bb7ff06cffc912
Alex Metelli <alex-metelli@gmx.com>
2024-10-14 15:58:38 +0800

    derived clone for BlockHeader

--- src/block_header.rs
+++ src/block_header.rs
@@ -33,7 +33,7 @@ use sha3::{Digest, Keccak256};
 /// - `blob_gas_used`: The amount of blob gas used, specific to blob transactions (optional).
 /// - `excess_blob_gas`: The excess blob gas present in the block (optional).
 /// - `parent_beacon_block_root`: The root of the parent beacon block, used in Ethereum's proof-of-stake chain (optional).
-#[derive(Debug)]
+#[derive(Clone,Debug)]
 pub struct BlockHeader {
     pub block_hash: String,               // character(66) NOT NULL
     pub number: i64,                      // bigint NOT NULL
